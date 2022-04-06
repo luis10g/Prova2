69e80726ca424e879e83129198de75a443fe0633
luis10g <joicefernandasouzaoliveira@gmail.com>
2022-04-06 17:55:46 +0000

    PROVA2

---
+++ Progressao.java
@@ -0,0 +1,15 @@
+public class Progressao {
+    public static void main(String[] args) {
+
+
+        int vetor1= 125;
+        int vetor2= 5;
+        
+        for(int i =0;i<10;i++) {
+            vetor1 = vetor1 * vetor2;
+
+            System.out.println(vetor1);
+        }
+    }
+}
+       
---
+++ Substitutiva.java
@@ -0,0 +1,11 @@
+public class Substitutiva {
+    public static void main(String[] args) {
+        int numero = 5;
+        int inicio = 10;
+        int tamanho = 6;
+
+        for(int i = inicio;i <(i + tamanho);i++) {
+            System.out.println(numero + " * " + i + " = " + (numero * i));
+        }
+    }
+}
